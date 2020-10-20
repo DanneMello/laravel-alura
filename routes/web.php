0cdfafe1168455e8100f7c3d7f65c48847d29e8d
Daniel Melo <melo.daniel00@gmail.com>
2020-10-20 01:59:38 -0300

    CURSO-LARAVEL - NEste capííitulo foi adicionado um menu e adicionado uma melhor validacao sobre o que o usuáario pode fazer caso nao esteja logado no sistema.

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,47 @@
 
 #Criando rotas.
 
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/series', 'SeriesController@index')
     ->name('listar_series');
 
 Route::get('/series/criar', 'SeriesController@create')
-    ->name('form_criar_serie');
+    ->name('form_criar_serie')
+    ->middleware('autenticador');
 
-Route::post('/series/criar', 'SeriesController@store'); // Rota para inserir série
-Route::delete('/series/{id}', 'SeriesController@destroy'); // Rota para remover série
-Route::post('/series/{id}/editaNome', 'SeriesController@editaNome'); // Rota para atualizar série
+Route::post('/series/criar', 'SeriesController@store')
+    ->middleware('autenticador'); // Rota para inserir série
+
+Route::delete('/series/{id}', 'SeriesController@destroy')
+    ->middleware('autenticador'); // Rota para remover série
+
+Route::post('/series/{id}/editaNome', 'SeriesController@editaNome')
+    ->middleware('autenticador'); // Rota para atualizar série
+
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 
 Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
-Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
 
+Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir')
+    ->middleware('autenticador');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/entrar', 'EntrarController@index');
+
 Route::post('/entrar', 'EntrarController@entrar');
 
 Route::get('/registrar', 'RegistroController@create');
+
 Route::post('/registrar', 'RegistroController@store');
+
+Route::get('/sair',
+    function () {
+        Auth::logout(); return redirect('/entrar');
+    }
+); // Faz logout
 
 
